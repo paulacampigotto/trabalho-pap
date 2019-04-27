7cdc2768a1ed488b7c901985fe5087bcdefc00d3
paulacampigotto <36995920+paulacampigotto@users.noreply.github.com>
2019-04-26 21:16:22 -0300

    Add files via upload

--- run.py
+++ run.py
@@ -6,31 +6,50 @@ import sys
 
 app = Flask(__name__)
 global retornoTemplate
+global cont
+cont = 0
 reload(sys)
 sys.setdefaultencoding('utf8')
-
 html = io.open('accessed.html', 'r+', encoding="utf-8")
 texto = html.read()
 html.close()
-
-
 retornoTemplate = texto
-
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route('/totalaccessed')
+def total():
+    global cont
+    return """
+
+    <!DOCTYPE html>
+    <html>
+    <body style="background-color:powderblue;">
+    	<style>
+    		div.a {
+    			text-align: center;
+    		}
+    	</style>
+    	<div class="a">
+    		<h2> """ + str(cont) + """ acessos nas catracas</h2>
+    		<br><br>
+    	</div>
+    </body>
+    </html>
+
+"""
+
 @app.route('/accessed')
 def something():
     global retornoTemplate
-
-
     return retornoTemplate
 
 @app.route('/getRegisters/nome=<string:nome>&cpf=<string:cpf>&tipo=<string:tipo>', methods=['GET', 'POST'])
 def req(nome, cpf, tipo):
     global retornoTemplate
+    global cont
     permissao = 'Professor'
     if(tipo == 2):
         permissao = 'Aluno'
@@ -49,5 +68,6 @@ def req(nome, cpf, tipo):
     html.close()
 
     retornoTemplate = texto[0] + linha+ texto[1]
+    cont =cont+ 1
 
     return "<h1> Requisicao recebida pelo servidor! </h1>"
